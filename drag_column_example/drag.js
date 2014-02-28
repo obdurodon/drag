372286c302bb0314b7da8e2b31fcf38560fef733
dbroglie <deb65@pitt.edu>
2014-02-28 09:30:12 -0500

    Column reordering functionality added
    Columns now rearrange on drop.  Need to add line functionality.

--- drag_column_example/drag.js
+++ drag_column_example/drag.js
@@ -12,24 +12,65 @@ function drop(ev) {
     cols = document.getElementsByClassName('col');
     colsLength = cols.length;
     
-    var x1 = cols[0].getBoundingClientRect().left;
-    var x2 = cols[1].getBoundingClientRect().left;
-    var interval = x2 - x1;
+    var xposArr = new Array(colsLength);
     
-    console.log(interval);
-    
-    for (i = 0; i < colsLength; i++) {
+    // add id and xpos to 2D array
+    for (i = 0; i < colsLength; i++) {        
         var id = cols[i].getAttribute('id');
         if (callId == id) {
-            console.log("column " + (i+1) + ": " + ev.clientX);
+            xposArr[i] = new Array();
+            xposArr[i][0] = id;
+            xposArr[i][1] = ev.clientX;
+            //console.log("column " + (i+1) + ": " + ev.clientX);
         } else {
             var rect = cols[i].getBoundingClientRect();
-            console.log("column " + (i+1) + ": " + rect.left);
+            
+            xposArr[i] = new Array();
+            xposArr[i][0] = id;
+            xposArr[i][1] = rect.left;
+            //console.log("column " + (i+1) + ": " + rect.left);
         }
     }
     
+    // sort columns on xpos
+    xposArr = xposArr.sort(function (a, b) {
+        return a[1] > b[1];
+    });
+    
+    // print sorted columns for debugging
+    for (i = 0; i < colsLength; i++) {
+        console.log(xposArr[i][0] + ": " + xposArr[i][1]);
+    }
+    
+    // assign columns data-index values
+    for (i = 0; i < colsLength; i++) {
+        elem = document.getElementById(xposArr[i][0]);
+        elem.setAttribute('data-index', alphabet[i]);
+    }
+    
+    // http://stackoverflow.com/questions/282670/easiest-way-to-sort-dom-nodes
+    var div = document.getElementById('drop');
+    
+    var items = div.childNodes;
+    var itemsArr = [];
+    for (var i in items) {
+        if (items[i].nodeType == 1) { // get rid of the whitespace text nodes
+            itemsArr.push(items[i]);
+        }
+    }
+
+    itemsArr.sort(function(a, b) {
+        return a.getAttribute('data-index') > b.getAttribute('data-index');
+    });
+
+    for (i = 0; i < itemsArr.length; ++i) {
+        div.appendChild(itemsArr[i]);
+    }
 }
             
 function dragOut(ev) {
     ev.target.style.borderColor="#aaaaaa";
 }
+
+var alphabet = new Array('a','b','c','d','e','f','g','h','i','j','k','l');
+
