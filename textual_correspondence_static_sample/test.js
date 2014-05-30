91deea731fd34c6ad5e534506c524765f6610388
David J Birnbaum <djbpitt@gmail.com>
2014-05-30 07:16:30 -0400

    Columns now swapping; To do: 1) snap to coordinates on drop, 2) stretch lines, and 3) redraw lines

--- textual_correspondence_static_sample/test.js
+++ textual_correspondence_static_sample/test.js
@@ -1,3 +1,9 @@
+/*
+ * Synopsis: Drag and drop support for plectograms
+ * Developer: David J. Birnbaum, djbpitt@gmail.com http://www.obdurodon.org
+ * Project: http://repertorium.obdurodon.org
+ * Date: First version 2014-05-30
+ */
 // drag and drop based on http://dl.dropboxusercontent.com/u/169269/group_drag.svg
 // see also http://www.codedread.com/dragtest2.svg
 function startMove(evt) {
@@ -9,8 +15,9 @@ function startMove(evt) {
     //target is <image> child of column, so need to move up two generations
     this.parentNode.parentNode.appendChild(newG);
     this.parentNode.parentNode.removeChild(this.parentNode);
-    var objectX = newG.getAttribute('transform').slice(10, -1);
-    console.log('startMove for object ' + newG.id + ' with objectX=' + objectX + ' and mouseX=' + mouseStartX);
+    //objectX is global, records starting position of drag
+    objectX = parseInt(newG.getAttribute('transform').slice(10, -1));
+    //console.log('startMove for object ' + newG.id + ' with objectX=' + objectX + ' and mouseX=' + mouseStartX);
     newG.getElementsByTagName('image')[0].addEventListener('mousedown', startMove, false);
     window.addEventListener('mousemove', moveIt, false);
     window.addEventListener('mouseup', endMove, false);
@@ -28,7 +35,7 @@ function startMove(evt) {
 function endMove(evt) {
     window.removeEventListener('mousemove', moveIt, false);
     newG = null;
-    console.log('endMove at mouse position ' + evt.clientX);
+    //console.log('endMove at mouse position ' + evt.clientX);
     eraseLines();
     var lines = document.getElementsByTagName('line');
     for (i = 0; i < lines.length; i++) {
@@ -40,13 +47,40 @@ function endMove(evt) {
     }
 }
 function moveIt(evt) {
-    var g = newG;
-    var oldObjectX = g.getAttribute('transform').slice(10, -1);
+    var oldObjectX = newG.getAttribute('transform').slice(10, -1);
     var newObjectX = parseInt(oldObjectX) + evt.clientX - mouseStartX;
-    console.log('Now moving ' + g.id + ' at ' + oldObjectX + ' to ' + newObjectX);
-    g.setAttribute('transform', 'translate(' + newObjectX + ')');
-    mouseStartX = evt.clientX;
+    //console.log('Now moving ' + newG.id + ' at ' + oldObjectX + ' to ' + newObjectX);
+    newG.setAttribute('transform', 'translate(' + newObjectX + ')');
     // global variable, initialized in startMove()
+    mouseStartX = evt.clientX;
+    if (newObjectX < objectX - spacing && newObjectX != '0') {
+        swapColumns('left', evt.clientX);
+    } else if (newObjectX > objectX + spacing && newObjectX < farRight) {
+        swapColumns('right', evt.clientX);
+    }
+}
+function swapColumns(side, mousePos) {
+    var newObjectX = parseInt(newG.getAttribute('transform').slice(10, -1));
+    var columns = document.getElementsByClassName('draggable');
+    if (side == 'left') {
+        for (var i = 0; i < columns.length; i++) {
+            var neighborPos = parseInt(columns[i].getAttribute('transform').slice(10, -1));
+            if (neighborPos == objectX - spacing) {
+                columns[i].setAttribute('transform', 'translate(' + objectX + ')');
+                objectX = objectX - spacing;
+            }
+        }
+    } else if (side == 'right') {
+        // must count backward to avoid destructive iteration
+        for (var i = 0; i < columns.length; i++) {
+            var neighborPos = parseInt(columns[i].getAttribute('transform').slice(10, -1));
+            if (neighborPos == objectX + spacing) {
+                columns[i].setAttribute('transform', 'translate(' + objectX + ')');
+                objectX = objectX + spacing;
+                break;
+            }
+        }
+    }
 }
 function drawLines() {
 }
@@ -63,6 +97,11 @@ function plectogram_init() {
     for (var i = 0; i < images.length; i++) {
         images[i].addEventListener('mousedown', startMove, false);
     }
+    //Intercolumn distance (global variable) used to determine when columns have crossed
+    var columns = document.getElementsByClassName('draggable');
+    spacing = parseInt(columns[1].getAttribute('transform').slice(10, -1)) - parseInt(columns[0].getAttribute('transform').slice(10, -1));
+    farRight = spacing * parseInt(document.getElementsByClassName('draggable').length);
+    console.log('Intercolumn spacing is ' + spacing + ' pixels');
     window.addEventListener('mouseup', endMove, false);
 }
 window.addEventListener('load', plectogram_init, false);
